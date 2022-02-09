f755954c06878e452bcf29a7db2c1478d1c12277
Exterminatorx99 <kristijan.kos99@gmail.com>
2022-02-09 19:14:00 +0100

    Fix for Data-oriented Tiles

--- LiquidWorld.cs
+++ LiquidWorld.cs
@@ -12,7 +12,7 @@ namespace LiquidWorld
 	public sealed class LiquidWorld : Mod
 	{
 		public static WeightedRandom<int> WeightedRandom = null!;
-		public static ushort? TileTypeToPlace;
+		public static ushort?             TileTypeToPlace;
 
 		public override void Load()
 		{
@@ -24,7 +24,7 @@ namespace LiquidWorld
 
 		public override void Unload()
 		{
-			WeightedRandom = null!;
+			WeightedRandom  = null!;
 			TileTypeToPlace = null;
 
 			ILWorldGen.KillTile -= LiquidEdit;
@@ -37,28 +37,37 @@ namespace LiquidWorld
 
 			Func<Instruction, bool>[] branchInstructions =
 			{
-				i => i.MatchLdloc(0),
-				i => i.MatchLdfld<Tile>(nameof(Tile.type)),
-				i => i.MatchLdcI4(TileID.Hellstone),
-				i => i.MatchBneUn(out _),
-				i => i.MatchLdarg(1),
-				i => i.MatchCall<Main>($"get_{nameof(Main.UnderworldLayer)}"),
-				i => i.MatchCgt(),
-				i => i.MatchBr(out _),
-				i => i.MatchLdcI4(0),
-				i => i.MatchStloc(99),
-				i => i.MatchLdloc(99),
-				i => i.MatchBrfalse(out _)
+				// if (tile.type == 58 && j > Main.UnderworldLayer)
+				i => i.MatchLdloca(0),               // IL_0af8: ldloca.s 0
+				i => i.MatchCall<Tile>("get_type"),  // IL_0afa: call instance uint16& Terraria.Tile::get_type()
+				i => i.MatchLdindU2(),               // IL_0aff: ldind.u2
+				i => i.MatchLdcI4(TileID.Hellstone), // IL_0b00: ldc.i4.s 58
+				i => i.MatchBneUn(out _),            // IL_0b02: bne.un.s IL_0b23
+
+				i => i.MatchLdarg(1),                          // IL_0b04: ldarg.1
+				i => i.MatchCall<Main>("get_UnderworldLayer"), // IL_0b05: call  int32 Terraria.Main::get_UnderworldLayer()
+				i => i.MatchBle(out _),                        // IL_0b0a: ble.s IL_0b23
+				// tile.lava(lava: true);
+				i => i.MatchLdloca(0),          // IL_0b0c: ldloca.s 0
+				i => i.MatchLdcI4(1),           // IL_0b0e: ldc.i4.1
+				i => i.MatchCall<Tile>("lava"), // IL_0b0f: call instance void Terraria.Tile::lava(bool)
+				// tile.liquid = 128;
+				i => i.MatchLdloca(0),                // IL_0b14: ldloca.s 0
+				i => i.MatchCall<Tile>("get_liquid"), // IL_0b16: call instance uint8& Terraria.Tile::get_liquid()
+				i => i.MatchLdcI4(128),               // IL_0b1b: ldc.i4 128
+				i => i.MatchStindI1(),                // IL_0b20: stind.i1
+				// else if (tile.type == 419)
+				i => i.MatchBr(out _), // IL_0b21: br.s IL_0b55
 			};
 
-			if (!c.TryGotoNext(branchInstructions))
+			if (!c.TryGotoNext(MoveType.Before, branchInstructions))
 			{
 				Logger.Fatal("Failed to find Hellstone 'branch` instructions");
 				return;
 			}
 
 			c.Emit(OpCodes.Ldloc_0);
-			c.Emit(OpCodes.Call, typeof(LiquidWorld).GetMethod(nameof(LiquidStuff)));
+			c.EmitDelegate(LiquidStuff);
 		}
 
 		private void AllowPlace(ILContext il)
@@ -67,12 +76,15 @@ namespace LiquidWorld
 
 			Func<Instruction, bool>[] breakInstructions =
 			{
-				i => i.MatchLdloc(0),
-				i => i.MatchLdcI4(0),
-				i => i.MatchStfld<Tile>(nameof(Tile.type)),
-				i => i.MatchLdloc(0),
-				i => i.MatchLdcI4(0),
-				i => i.MatchCallvirt<Tile>("inActive")
+				// tile.type = 0;
+				i => i.MatchLdloca(0),              // IL_0b55: ldloca.s 0
+				i => i.MatchCall<Tile>("get_type"), // IL_0b57: call instance uint16& Terraria.Tile::get_type()
+				i => i.MatchLdcI4(0),               // IL_0b5c: ldc.i4.0
+				i => i.MatchStindI2(),              // IL_0b5d: stind.i2
+				// tile.inActive(inActive: false);
+				i => i.MatchLdloca(0),              // IL_0b5e: ldloca.s 0
+				i => i.MatchLdcI4(0),               // IL_0b60: ldc.i4.0
+				i => i.MatchCall<Tile>("inActive"), // IL_0b61: call instance void Terraria.Tile::inActive(bool)
 			};
 
 			if (!c.TryGotoNext(MoveType.After, breakInstructions))
@@ -83,7 +95,7 @@ namespace LiquidWorld
 
 			c.Emit(OpCodes.Ldarg_0);
 			c.Emit(OpCodes.Ldarg_1);
-			c.Emit(OpCodes.Call, typeof(LiquidWorld).GetMethod(nameof(PlaceTile)));
+			c.EmitDelegate(PlaceTile);
 		}
 
 		public static void LiquidStuff(Tile tile)
@@ -119,7 +131,7 @@ namespace LiquidWorld
 						LiquidID.Lava when liquid2 is LiquidID.Honey => TileID.CrispyHoneyBlock,
 						LiquidID.Honey when liquid2 is LiquidID.Lava => TileID.CrispyHoneyBlock,
 
-						_ => null
+						_ => null,
 					};
 
 					if (tileType is { } type)
@@ -163,10 +175,10 @@ namespace LiquidWorld
 			for (int _ = 0; _ < count; _++)
 			{
 				int npcType = Main.rand.Next(210, 212);
-				NPC npc = Main.npc[NPC.NewNPC(i * 16 + 8, j * 16 + 15, npcType, 1)];
+				NPC npc     = Main.npc[NPC.NewNPC(i * 16 + 8, j * 16 + 15, npcType, 1)];
 				npc.velocity.X = Main.rand.Next(-200, 201) * 0.002f;
 				npc.velocity.Y = Main.rand.Next(-200, 201) * 0.002f;
-				npc.netUpdate = true;
+				npc.netUpdate  = true;
 			}
 		}
 
